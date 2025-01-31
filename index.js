a3968f2bc1981ce8817a54f5ffa51d21645feb3b
simee56 <pandeysimee7@gmail.com>
2025-01-31 21:41:25 +0530

    ejs update

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require('express');
+const path = require('path');
 const { connectMongoDB } = require('./connection');
 const urlRoute = require('./routes/url');
 
 const URL = require('./models/url')
 
 const app = express();
-const PORT = 8003;
+const PORT = 8001;
 
 connectMongoDB("mongodb://127.0.0.1:27017/short-url").then(() =>
     console.log("MongoDb connected")
 );
 
 app.set("view engine", "ejs");
+app.set("views", path.resolve('./views'));
 
 app.use(express.json());
+
+app.get('/test', async(req, res) => {
+    const allUrls = await URL.find({});
+    return res.render('home', {               //this home is our ejs file
+        urls : allUrls
+    })   
+});
+
 app.use('/url', urlRoute);
 
 app.get("/:shortId", async(req, res) => {
