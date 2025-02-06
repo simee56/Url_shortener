f21de96103c637dafbe1d7a3d6aa2deb834a67ee
simee56 <pandeysimee7@gmail.com>
2025-02-06 21:03:47 +0530

    create static route

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require('express');
+
 const path = require('path');
-const { connectMongoDB } = require('./connection');
+
+const connectMongoDB = require('./connection');
+
 const urlRoute = require('./routes/url');
+
 const staticRoute = require('./routes/staticRouter');
+
 const URL = require('./models/url');
 
 const app = express();
-const PORT = 8001;
 
-connectMongoDB("mongodb://127.0.0.1:27017/short-url").then(() =>
+const PORT = 8000;
+
+connectMongoDB("mongodb://127.0.0.1:27017/short-url").then(() =>   //here then is a listener. If mongodb get connected then print MongoDb connected
     console.log("MongoDb connected")
 );
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve('./views'));
 
+//middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
@@ -27,13 +34,12 @@ app.get('/test', async (req, res) => {
 
 app.use('/url', urlRoute);
 
-app.use('/', staticRoute);
+app.use('/', staticRoute);    // staric router = HTML ke rendering ke liye
 
 app.get("/:shortId", async (req, res) => {
     const shortId = req.params.shortId;
-    const entry = await URL.findOneAndUpdate({
-        shortId
-    },
+    const entry = await URL.findOneAndUpdate(
+        { shortId },
         {
             $push: {
                 visitHistory: {
@@ -42,7 +48,6 @@ app.get("/:shortId", async (req, res) => {
             }
         }
     )
-
     if (!entry) {
         return res.status(404).json({ error: "Short URL not found" });
     }
