0cf4c222d79da377260f6b4d5c1d4b3d772fa4a8
simee56 <pandeysimee7@gmail.com>
2025-02-06 21:04:17 +0530

    update connections

--- connection.js
+++ connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+
 mongoose.set("strictQuery", true);
 
 async function connectMongoDB(url) {
     return mongoose.connect(url);
 }
 
-module.exports = { connectMongoDB };
+module.exports =  connectMongoDB ;
