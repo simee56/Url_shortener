c1a41efa271c27a1dca363f4f181d07cbfb1d158
simee56 <pandeysimee7@gmail.com>
2025-02-07 22:15:42 +0530

    create login page

--- controllers/user.js
+++ controllers/user.js
@@ -9,9 +9,20 @@ async function handleUserSignUp(req, res) {
         mobile,
         gender
     });
-    return res.render("home");
+    return res.redirect("/");
+}
+
+async function handleUserLogIn(req, res) {
+    const {  email, password } = req.body;
+   const user =await User.findOne({email, password});
+   if(!user)
+    return res.render("login",{
+        error: "Invalid email or password"
+    });
+    return res.redirect('/');
 }
 
 module.exports = {
     handleUserSignUp,
+    handleUserLogIn
 }
