44e236aea43842f99a2d4b1820df869db24cc8e1
simee56 <pandeysimee7@gmail.com>
2025-02-09 21:37:39 +0530

    import jwt

--- services/auth.js
+++ services/auth.js
@@ -1,7 +1,12 @@
-const sessionIdToUserMap = new Map();
+const jwt = require('jsonwebtoken');
+const secretKey = "abpusa$6$+1";
 
-function setUser(id, user) {
-    sessionIdToUserMap.set(id, user)
+function setUser(id, user) {   //this function will create tokens 
+   const payLoad = {
+    id,
+    ...user,
+   }
+   return jwt.sign(payLoad,secretKey)
 };
 
 function getUser(id) {
