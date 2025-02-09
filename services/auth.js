97d30044c522fcb566bd9483e197f4e189998143
simee56 <pandeysimee7@gmail.com>
2025-02-09 23:28:54 +0530

    update getUser

--- services/auth.js
+++ services/auth.js
@@ -1,16 +1,25 @@
 const jwt = require('jsonwebtoken');
 const secretKey = "abpusa$6$+1";
 
-function setUser( user) {   //this function will create tokens 
-   return jwt.sign(user,secretKey)
+function setUser(user) {   //this function will create tokens 
+    return jwt.sign({
+        _id: user.id,
+        email: user.email
+    },
+        secretKey);
 };
 
 function getUser(token) {
-    if(!token)
+    if (!token) return null;
+
+    try {
+        return jwt.verify(token, secretKey);
+    } catch (err) {
+        console.error("JWT verification failed:", err);
         return null;
-    
-    return jwt.verify(token,secretKey)
+    }
 };
+
 
 module.exports = {
     setUser,
