6aba24d30c8b7ff0184fa6998b2791bc36a8af95
simee56 <pandeysimee7@gmail.com>
2025-02-12 14:33:35 +0530

    correction in middlewares

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,33 +1,33 @@
 const { getUser } = require('../services/auth');
 
+//AUTHENTICATION
 function checkForAuthentication(req, res, next) {
 
-    const authorizationHeaderValue = req.headers['authorization'];
+    const tokenCookie = req.cookies?.token;   //checks if there any cookie exists
     req.user = null;
 
-    if (!authorizationHeaderValue || !authorizationHeaderValue.startsWith("Bearer")) {
-        return next();
-    }
-    const token = authorizationHeaderValue.split("Bearer ")[1];
+    if (!tokenCookie) return next();
+    
+    const token = tokenCookie;
     const user = getUser(token);
 
     req.user = user;
     return next();
 }
 
+//AUTHORIZATION
 function restrictTo(roles = []) {
     return function (req, res, next) {
-        if (!req.user) {
-            return res.redirect("/login");
-        }
-        if (!roles.includes(req, res, next)) {
+        if (!req.user) return res.redirect("/login");
+
+        if (!roles.includes(req.user.role)) {
             return res.end("UnAuthorized");
         }
-
         return next();
     }
 }
 
 module.exports = {
-    checkForAuthentication
+    checkForAuthentication,
+    restrictTo
 }
